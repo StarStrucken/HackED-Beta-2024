338a77f1a43561101ee2e1ce2082d3260db2c06d
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2024-11-09 13:41:10 -0700

    coolio

--- Frontend/src/NavBar.jsx
+++ Frontend/src/NavBar.jsx
@@ -2,8 +2,17 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { DiCodeigniter } from "react-icons/di";
-
+import NavDropdown from "react-bootstrap/NavDropdown";
 export default function NavBar() {
+    const showTeam = () => {
+        console.log("team");
+    }
+
+    const showTechStack = () => {
+        console.log("tech stack");
+    }
+
+
   return (
     <>
       <Navbar
@@ -11,7 +20,7 @@ export default function NavBar() {
         style={{
           backgroundColor: "linear-gradient(#6441a5, #2a0845)",
           color: "black",
-          padding: "10px 20px",
+          padding: "5px 10px",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
@@ -21,12 +30,18 @@ export default function NavBar() {
         <div style={{ display: "flex", alignItems: "center" }}>
         <Nav>
           <Nav.Link href="/" className="nav-link">
-            <DiCodeigniter size={40} style={{ width: "30px"}}/>
+            <DiCodeigniter size={40} style={{ width: "30px", marginLeft: "10px"}}/>
           </Nav.Link>
 
           <Nav.Link href="/">
-            <p style={{ width: "250px", fontSize: "30px", fontFamily: "monospace" }}>IDC</p>
+            <p style={{ width: "250px", fontSize: "30px", fontFamily: "monospace"}}>IDC</p>
           </Nav.Link>
+
+          <NavDropdown title="About" id="about-dropdown">
+            <NavDropdown.Item onclick={showTeam}>Team</NavDropdown.Item>
+            <NavDropdown.Item onclick={showTechStack}>Tech Stack</NavDropdown.Item>
+          </NavDropdown>
+        
         </Nav>
         </div>
             
--- Frontend/src/StartPage.jsx
+++ Frontend/src/StartPage.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import "./StartPage.css"
 import "./DataVisPage"
 import NavBar from "./NavBar";
-import DataVisPage from "./DataVisPage";
 
 export default function StartPage() {
     return (
-        
         <div className="page">
             <NavBar />
             <p className="project-title">Global Renewable Energy Forecasting and Optimization Tool</p>
