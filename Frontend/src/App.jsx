5bea56d49cc6d58da5ff7b6e1aede12e16f91f70
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2024-11-09 11:15:20 -0700

    Added Frontend and Backend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import StartPage from './StartPage';
+
+function App() {
+  return (  
+    <div className="App">
+      <StartPage />
+  </div>
+  )
+}
+
+export default App;
---
+++ Frontend/src/StartPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StartPage() {
+    return (
+        <div>
+            <h1>StartPage</h1>
+        </div>
+    );
+}
