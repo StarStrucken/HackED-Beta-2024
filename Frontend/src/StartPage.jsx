1a1d3b5398050574a866f1737baa46e072859293
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2024-11-09 11:47:27 -0700

    added stuffs

--- Frontend/src/StartPage.jsx
+++ Frontend/src/StartPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./StartPage.css"
 
 export default function StartPage() {
     return (
-        <div>
-            <h1>StartPage</h1>
+        <div className="page">
+            <h1 className="project-title">Global Renewable Energy Forecasting and Optimization Tool</h1>
         </div>
-    );
+    )
 }
